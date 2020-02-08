61a322b7e72fcac6b63ed94a57521ad61bacc369
Dmitry Yurov <dm.yurov@gmail.com>
2020-02-08 12:34:47 +0100

    Extend evalarray
    1. Allow inheritance
    2. Add construction from plain arrays
    3. Add access operator
    4. Add size method

--- evalarray/evalarray.h
+++ evalarray/evalarray.h
@@ -4,13 +4,21 @@
 #include <cstddef>
 
 template<class T>
-class evalarray final {
+class evalarray {
 public:
+    // constructors
     evalarray();
     evalarray(const evalarray<T>& other);
     evalarray(evalarray<T>&& other);
+    evalarray(const T* plain_array, size_t count);
 
-    ~evalarray();
+    virtual ~evalarray();
+
+    // operators
+    T& operator[](size_t index) { return m_array[index]; }
+    const T& operator[](size_t index) const { return m_array[index]; }
+
+    std::size_t size() const { return m_size; }
 
 private:
     std::size_t m_size;
@@ -22,6 +30,15 @@ evalarray<T>::evalarray()
     : m_size(0)
     , m_array(nullptr)
 {}
+
+template<class T>
+evalarray<T>::evalarray(const T* plain_array, size_t count)
+    : m_array(new T[count])
+    , m_size(count)
+{
+    for (size_t i = 0; i < m_size; ++i)
+        m_array[i] = plain_array[i];
+}
 
 template<class T>
 evalarray<T>::~evalarray()
