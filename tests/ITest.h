c98c7c21545462aa76cbf89249e78faaaab76da1
Dmitry Yurov <dm.yurov@gmail.com>
2020-02-15 23:02:35 +0100

    Print test case status

--- tests/ITest.h
+++ tests/ITest.h
@@ -1,6 +1,8 @@
 #ifndef ITEST_H_
 #define ITEST_H_
 
+#include <iostream>
+
 #include "TestContainer.h"
 
 class ITest {
@@ -13,17 +15,30 @@ public:
     };
 
     virtual ~ITest() = default;
+    virtual const char* title() const = 0;
     virtual bool run() = 0;
+
+    bool execute()
+    {
+        std::cout << title() << ": ";
+        bool result = run();
+        std::cout << (result ? "Ok" : "Failed") << std::endl;
+
+        return result;
+    }
 };
 
 #define ADD_TEST(name) \
 class name : public ITest { \
 public: \
     ~name() override = default; \
+    const char* title() const override; \
     bool run() override; \
 }; \
 \
 const ITest::AddTestHelper<name> name ## var; \
+\
+const char* name::title() const {return #name;} \
 \
 bool name::run()
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -6,7 +6,7 @@ int main(int, char**)
 {
     int count = 0; // number of failed test cases
     for (auto& test: TestContainer::tests())
-        count += !test->run();
+        count += !test->execute();
 
     std::cout << count << " tests failed" << std::endl;
     return count;
